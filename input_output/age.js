03dc3877e121d5668d51a363c7c1cb4f62102fa4
Wonnie Ki <wonnie.tech@gmail.com>
2023-11-20 17:39:08 -0800

    Change to what the solution showed

--- input_output/age.js
+++ input_output/age.js
@@ -1,6 +1,6 @@
 let readlineSync = require('readline-sync');
 let age = readlineSync.question('How old are you?\n');
-age = Number(age);
+age = parseInt(age);
 
 console.log(`You are ${age} years old.`);
 console.log(`In 10 years, you will be ${age + 10} years old.`);
