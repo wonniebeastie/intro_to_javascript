a0f0449842bacb3446bcb1dfb40221569a7f2f9c
Wonnie Ki <wonnie.tech@gmail.com>
2023-11-21 11:42:41 -0800

    Update exercise for Arguments & Parameters

--- functions/say2.js
+++ functions/say2.js
@@ -1,4 +1,8 @@
-console.log("hello");
-console.log("hi");
-console.log("how do you do");
-console.log("Quite all right");
+function say(text) {
+  console.log(text);
+}
+
+say("hello");
+say("hi");
+say("how do you do");
+say("Quite all right");
