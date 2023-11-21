c29a23d7105e3b407eeaf65948efc31741c6bfa8
Wonnie Ki <wonnie.tech@gmail.com>
2023-11-20 17:36:48 -0800

    Change to try the solution showed but with Number()

--- input_output/age.js
+++ input_output/age.js
@@ -1,8 +1,9 @@
 let readlineSync = require('readline-sync');
 let age = readlineSync.question('How old are you?\n');
+age = Number(age);
 
 console.log(`You are ${age} years old.`);
-console.log(`In 10 years, you will be ${Number(age) + 10} years old.`);
-console.log(`In 20 years, you will be ${Number(age) + 20} years old.`);
-console.log(`In 30 years, you will be ${Number(age) + 30} years old.`);
-console.log(`In 40 years, you will be ${Number(age) + 40} years old.`);
+console.log(`In 10 years, you will be ${age + 10} years old.`);
+console.log(`In 20 years, you will be ${age + 20} years old.`);
+console.log(`In 30 years, you will be ${age + 30} years old.`);
+console.log(`In 40 years, you will be ${age + 40} years old.`);
