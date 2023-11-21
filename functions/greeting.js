db5eec7e3c3339ffd832d321eb65e4a293d7ae71
Wonnie Ki <wonnie.tech@gmail.com>
2023-11-21 14:30:54 -0800

    Change file for local variable demonstration

--- functions/greeting.js
+++ functions/greeting.js
@@ -1,12 +1,6 @@
-let greetingMessage = "Good Morning!";
 
 function greetPeople() {
+  let greetingMessage = "Good Morning!";
   console.log(greetingMessage);
 }
-
-function changeGreetingMessage(newMessage) {
-  greetingMessage = newMessage;
-}
-
-changeGreetingMessage("Good Evening");
 greetPeople();
