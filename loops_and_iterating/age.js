48d78ec66b0c0f0ce5f258f6a48645598d6e6320
Wonnie Ki <wonnie.tech@gmail.com>
2023-12-13 16:45:30 -0800

    Attempt at exercise #1

--- loops_and_iterating/age.js
+++ loops_and_iterating/age.js
@@ -1,4 +1,5 @@
 let readlineSync = require('readline-sync');
+/*
 let age = readlineSync.question('How old are you?\n');
 age = parseInt(age);
 
@@ -7,3 +8,19 @@ console.log(`In 10 years, you will be ${age + 10} years old.`);
 console.log(`In 20 years, you will be ${age + 20} years old.`);
 console.log(`In 30 years, you will be ${age + 30} years old.`);
 console.log(`In 40 years, you will be ${age + 40} years old.`);
+*/
+
+let age = readlineSync.question('How old are you?\n');
+age = parseInt(age);
+let future = 10;
+
+console.log(`You are ${age} years old.`);
+
+
+for (let i = 1; i <= 4; i++) {
+
+  console.log(`In ${future} years, you'll be ${age + future} years old.`);
+
+  future += 10;
+  age += 10;
+}
